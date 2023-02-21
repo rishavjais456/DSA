b40161f4ec47f4da2a62a5490d07a5af3dfc3dc6
rishavjais456 <rishavjais456@gmail.com>
2023-02-21 10:58:55 +0530

    matrix rotation

--- matrix_rotation_90.c
+++ matrix_rotation_90.c
@@ -11,7 +11,7 @@ int main()
     }
    }
    for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){    /// comment
+    for(int j=0;j<n;j++){    //S commenSt
         printf("%d ",arr[i][j]);
     }
     printf("\n");
